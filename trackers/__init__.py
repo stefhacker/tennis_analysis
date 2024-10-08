297c8757a8e6936b4c7563fdac85a500be8545c1
hacker00 <stefhaecker@gmail.com>
2024-10-08 20:34:27 +0200

    "some additons"

---
+++ trackers/__init__.py
@@ -0,0 +1,1 @@
+from .player_tracker import PlayerTracker #dot indicating PlayerTracker is located in the same directory
---
+++ trackers/player_tracker.py
@@ -0,0 +1,31 @@
+from ultralytics import YOLO
+
+class PlayerTracker:
+    def __init__(self, model_path):
+        self.model = YOLO(model_path)
+    
+    def detect_frames(self, frames):
+        player_detections = []
+        for frame in frames:
+            player_dict = self.detect_frame(frame)
+            player_detections.append(player_dict)
+        return player_detections
+   
+
+    def detect_frame(self, frame):
+        results = self.model.track(frame, persist=True)[0] #persist for tracking, even tough we give individual frame at once
+        id_name_dict = results.names
+
+        player_dict = {}
+        for box in results.boxes:
+            track_id = int(box.id.tolist()[0])
+            result = box.xyxy.tolist()[0]
+            object_cls_id = box.cls.tolist()[0]
+            object_cls_name = id_name_dict[object_cls_id]
+            if object_cls_name == "person":
+                player_dict[track_id] = result
+
+
+
+
+    
