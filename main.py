cc02d489d525daddae72e522f657135b062d92e8
hacker00 <stefhaecker@gmail.com>
2024-10-08 21:04:08 +0200

    "some changes"

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from utils import (read_video,
                    save_video)
+from trackers import PlayerTracker
 
 def main():
+   # read video
    input_video_path = "input_videos/vid.mp4"
    video_frames = read_video(input_video_path)
 
-   save_video(video_frames, "output_videos/output_video.avi")
+   player_tracker = PlayerTracker(model_path='yolov8x')
+   player_detections = player_tracker.detect_frames(video_frames)
+
+   output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
+
+   save_video(output_video_frames, "output_videos/output_video.avi")
 
 if __name__ == "__main__":
     main()   
--- trackers/player_tracker.py
+++ trackers/player_tracker.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
+import cv2
 
 class PlayerTracker:
     def __init__(self, model_path):
@@ -18,12 +19,28 @@ class PlayerTracker:
 
         player_dict = {}
         for box in results.boxes:
-            track_id = int(box.id.tolist()[0])
+            if box.id is not None:
+                track_id = int(box.id.tolist()[0])
+            else:
+                continue  # Skip if there's no track ID
+                
             result = box.xyxy.tolist()[0]
             object_cls_id = box.cls.tolist()[0]
             object_cls_name = id_name_dict[object_cls_id]
             if object_cls_name == "person":
                 player_dict[track_id] = result
+        return player_dict
+
+    def draw_bboxes(self, video_frames, player_detections):
+        output_video_frames = []
+        for frame, player_dict in zip(video_frames, player_detections):
+            # draw bounding boxes
+            for track_id, bbox in player_dict.items():
+                x1, y1, x2 ,y2 = bbox
+                cv2.putText(frame,  f"Player ID: {track_id}", (int(bbox[0]), int(bbox[1] - 10)),  cv2.FONT_HERSHEY_SIMPLEX,  0.8,  (0, 0, 255),  2)
+                cv2.rectangle(frame, (int(x1),int(y1)), (int(x2), int(y2)), (0, 0, 255), 2) #2 for outside borders
+            output_video_frames.append(frame)
+        return(output_video_frames)
 
 
 
